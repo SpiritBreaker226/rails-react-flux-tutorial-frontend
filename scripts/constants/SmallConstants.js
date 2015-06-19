c9a0ce24944c5b6b13eae14e61ceb90c2592a73b
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-19 18:36:20 -0400

    Fix Constant STORY
    Fix the constant STORY as it is misspelled, missing a colon and using a
    period instead of a comma.

--- scripts/constants/SmallConstants.js
+++ scripts/constants/SmallConstants.js
@@ -22,8 +22,8 @@ module.exports = {
 
 		LOAD_STORIES: null,
 		RECEIVE_STORIES: null,
-		LOAD_STORYL null,
-		RECEIVE_STORY: null.
+		LOAD_STORY: null,
+		RECEIVE_STORY: null,
 		CREATE_STORY: null,
 		RECEIVE_CREATED_STORY: null
 	})
